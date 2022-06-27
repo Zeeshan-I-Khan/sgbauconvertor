efbab57b06ef220ae8674901c72e2b7a5610cfe9
Zeeshan-I-Khan <91727548+Zeeshan-I-Khan@users.noreply.github.com>
2022-06-27 13:35:48 +0530

    Add files via upload

---
+++ sample.js
@@ -0,0 +1,29 @@
+var a=10;
+var b=20;
+var c=div(a,b);
+var x,y;
+
+function sub(x,y)
+{
+var result=x-y;
+return result;
+}
+
+function mul(x,y)
+{
+var result=x*y;
+return result;
+}
+
+function div(x,y)
+{
+var result=x/y;
+return result;
+}
+
+function add(x,y)
+{
+var result=x+y;
+return result;
+}
+console.log(c);
